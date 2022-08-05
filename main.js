6b5cbe726e4d3fa58178a2b8e7b8b34a18542496
PXProcyon <61347741+PXProcyon@users.noreply.github.com>
2022-08-05 18:37:51 +0500

    /

---
+++ main.js
@@ -0,0 +1,8 @@
+var movie = "Fight Club";
+let description = "Терзаемый хронической бессонницей и отчаянно пытающийся вырваться из мучительно скучной жизни, клерк встречает некоего Тайлера Дардена, харизматического торговца мылом с извращенной философией."
+
+document.getElementById("movie").innerHTML = movie;
+//document.getElementById("description").innerHTML = description;
+
+$("#description").text(description);
+
